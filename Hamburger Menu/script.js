d57cdf203e3d486e0de4fa89cf5695f87ddfbcbf
JEmbry2019 <57116354+JEmbry2019@users.noreply.github.com>
2019-11-17 10:17:51 -0500

    hamburger menu toggle problem

---
+++ Hamburger Menu/script.js
@@ -0,0 +1,8 @@
+function toggleClass() {
+    let menu = document.querySelector("mainMenu");
+    menu.classList.toggle("toggleCls");
+}
+
+let hamburger = document.querySelector("hamIcon");
+
+hamburger.addEventListener("click", toggleClass);
